137c38dcfd3de9b8d0dd09aa763e48869a8577e3
Maximilian Schmidt <max.schmidt@fz-juelich.de>
2016-12-02 19:41:01 +0100

    Adapt tests to new function names

--- test_wrapper.py
+++ test_wrapper.py
@@ -62,29 +62,29 @@ class WrapperTest(unittest.TestCase):
 
     def test_write_and_load_with_label(self):
         res = self.construct_simpledata()
-        h5w.add_to_h5(fn, res, write_mode='w', dict_label='test_label')
+        h5w.save(fn, res, write_mode='w', dict_label='test_label')
         for key, val in zip(simpledata_str, simpledata_val):
-            self.assertEqual(h5w.load_h5(fn, 'test_label/' + key), val)
+            self.assertEqual(h5w.load(fn, 'test_label/' + key), val)
 
     def test_store_and_load_dataset_directly(self):
         res = self.construct_simpledata()
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         for key, val in zip(simpledata_str, simpledata_val):
-            self.assertEqual(h5w.load_h5(fn, '/' + key), val)
+            self.assertEqual(h5w.load(fn, '/' + key), val)
 
     def test_old_store_and_load_simpledata(self):
         res = self.construct_simpledata()
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         for key, val in zip(simpledata_str, simpledata_val):
             self.assertEqual(res[key], val)
 
     def test_store_and_load_simpledata(self):
         res = self.construct_simpledata()
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         for key, val in zip(simpledata_str, simpledata_val):
             self.assertEqual(res[key], val)
 
@@ -92,9 +92,9 @@ class WrapperTest(unittest.TestCase):
         res = {}
         for key, val in zip(arraydata_str, arraydata_val):
             res[key] = val
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         for key, val in zip(arraydata_str, arraydata_val):
             assert_array_equal(res[key], val)
 
@@ -102,9 +102,9 @@ class WrapperTest(unittest.TestCase):
         res = {}
         for key, val in zip(listdata_str, listdata_val):
             res[key] = val
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         for key, val in zip(listdata_str, listdata_val):
             assert_array_equal(res[key], val)
 
@@ -112,9 +112,9 @@ class WrapperTest(unittest.TestCase):
         res = {}
         for key, val in zip(tupledata_str, tupledata_val):
             res[key] = val
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         for key, val in zip(tupledata_str, tupledata_val):
             assert_array_equal(res[key], np.array(val))
 
@@ -122,44 +122,44 @@ class WrapperTest(unittest.TestCase):
         res = {}
         for key, val in zip(dictdata_str, dictdata_val):
             res[key] = val
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         for dkey, dval in zip(dictdata_str, dictdata_val):
             for key, val in dval.items():
                 self.assertEqual(res[dkey][key], val)
 
     def test_overwrite_dataset(self):
         res = {'a': 5}
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
         res = {'a': 6}
-        self.assertRaises(KeyError, h5w.add_to_h5,
+        self.assertRaises(KeyError, h5w.save,
                           fn, res, write_mode='a', overwrite_dataset=False)
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         self.assertEqual(res['a'], 5)  # dataset should still contain old value
         res.clear()
         res = {'a': 6}
-        h5w.add_to_h5(
+        h5w.save(
             fn, res, write_mode='a', overwrite_dataset=True)
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         self.assertEqual(res['a'], 6)  # dataset should contain new value
 
     def test_write_empty_array(self):
         res = {'a': [], 'b': np.array([])}
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         assert_array_equal(res['a'], [])
         assert_array_equal(res['b'], [])
 
     def test_write_nested_empty_array(self):
         res = {'a': [[], []], 'b': np.array([[], []])}
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         assert_array_equal(res['a'], [[], []])
         self.assertEqual(np.shape(res['a']), (2, 0))
         assert_array_equal(res['b'], [[], []])
@@ -167,49 +167,49 @@ class WrapperTest(unittest.TestCase):
 
     def test_read_empty_array_via_path(self):
         res = {'a': np.array([[], []])}
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn, path='a')
+        res = h5w.load(fn, path='a')
         assert_array_equal(res, [[], []])
         self.assertEqual(np.shape(res), (2, 0))
 
     def test_handle_nonexisting_path(self):
         res = {}
         stest = 'this is a test'
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         try:
-            res = h5w.load_h5(fn, path='test/')
+            res = h5w.load(fn, path='test/')
             raise Exception()  # should not get until here
         except KeyError:
             res['test'] = stest
-            h5w.add_to_h5(fn, res)
+            h5w.save(fn, res)
             res.clear()
-            res = h5w.load_h5(fn, path='test/')
+            res = h5w.load(fn, path='test/')
             self.assertEqual(res, stest)
 
     def test_store_none(self):
         res = {'a1': None}
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         self.assertIsNone(res['a1'])
 
     def test_handle_nonexisting_file(self):
         try:
-            h5w.load_h5('asdasd.h5')
+            h5w.load('asdasd.h5')
             raise Exception()  # should not get until here
         except IOError:
             pass
 
     def test_store_and_load_custom_array(self):
         a = [[1, 2, 3, 4], [6, 7]]
-        h5w.add_to_h5(fn, {'a': a}, overwrite_dataset=True)
+        h5w.save(fn, {'a': a}, overwrite_dataset=True)
         # loading the whole data
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         for i in xrange(len(a)):
             self.assertLess(np.sum(a[i] - res['a'][i]), 1e-12)
         # loading path directly
-        res = h5w.load_h5(fn, path='a/')
+        res = h5w.load(fn, path='a/')
         for i in xrange(len(a)):
             self.assertLess(np.sum(a[i] - res[i]), 1e-12)
 
@@ -217,22 +217,22 @@ class WrapperTest(unittest.TestCase):
     def test_store_and_load_quantities_array(self):
         data = {'times': np.array([1, 2, 3]) * pq.ms, 'positions':
                 np.array([1, 2, 3]) * pq.cm}
-        h5w.add_to_h5(fn, data, overwrite_dataset=True)
+        h5w.save(fn, data, overwrite_dataset=True)
         # loading the whole data
-        res = h5w.load_h5(fn)
+        res = h5w.load(fn)
         self.assertEqual(res['times'].dimensionality,
                          data['times'].dimensionality)
 
     def test_store_and_load_with_compression(self):
         data = {'a': 1, 'test1': {'b': 2}, 'test2': {
             'test3': {'c': np.array([1, 2, 3])}}}
-        h5w.add_to_h5(fn, data, write_mode='w', compression='gzip')
-        h5w.load_h5(fn)
+        h5w.save(fn, data, write_mode='w', compression='gzip')
+        h5w.load(fn)
 
     def test_store_and_test_key_types(self):
         data = {'a': 1, (1, 2): {4: 2.}, 4.: 3.}
-        h5w.add_to_h5(fn, data, write_mode='w')
-        res = h5w.load_h5(fn)
+        h5w.save(fn, data, write_mode='w')
+        res = h5w.load(fn)
 
         keys = ['a', (1, 2), 4.]
         for k in keys:
@@ -241,18 +241,18 @@ class WrapperTest(unittest.TestCase):
 
     def test_load_lazy_simple(self):
         res = self.construct_simpledata()
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn, lazy=True)
+        res = h5w.load(fn, lazy=True)
         for key, obj in res.items():
             self.assertIsNone(obj)
 
     def test_load_lazy_nested(self):
         res = {'a': 1, 'test1': {'b': 2}, 'test2': {
             'test3': {'c': np.array([1, 2, 3])}}}
-        h5w.add_to_h5(fn, res, write_mode='w')
+        h5w.save(fn, res, write_mode='w')
         res.clear()
-        res = h5w.load_h5(fn, lazy=True)
+        res = h5w.load(fn, lazy=True)
         self.assertIsNone(res['a'])
         self.assertIsNone(res['test1']['b'])
         self.assertIsNone(res['test2']['test3']['c'])
